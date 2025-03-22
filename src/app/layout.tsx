02e10d2e8aad9942a743429fa5156394634ab17b
R <rgembalik@users.noreply.github.com>
2025-03-22 23:27:38 +0100

    Fix incorrect link import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 import { Sparkles } from "lucide-react";
 import { Navigation } from "@/components/Navigation";
+import Link from "next/link";
 
 export const metadata = {
   title: "Happy Changelog",
@@ -20,14 +21,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <div className="flex min-h-screen flex-col">
             <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
               <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-                <div className="flex gap-2 items-center text-xl font-bold">
+                <Link
+                  href="/"
+                  className="flex gap-2 items-center text-xl font-bold block"
+                >
                   <Sparkles className="h-6 w-6 text-happy-500 animate-pulse-gentle" />
                   <span className="text-gradient">Happy Changelog</span>
-                </div>
+                </Link>
                 <div className="flex flex-1 items-center justify-end space-x-4">
                   <Navigation />
                 </div>
@@ -36,10 +40,13 @@ export default function RootLayout({
             <main className="flex-1">{children}</main>
             <footer className="w-full border-t py-6 md:py-0 bg-gradient-to-r from-happy-50 to-lavender-50 dark:from-happy-900/30 dark:to-lavender-900/30 dark:border-happy-800">
               <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-                <div className="flex gap-2 items-center text-lg font-semibold">
+                <Link
+                  href="/"
+                  className="cursor-pointer flex gap-2 items-center text-lg font-semibold"
+                >
                   <Sparkles className="h-5 w-5 text-happy-500 dark:text-happy-400" />
                   <span className="text-gradient">Happy Changelog</span>
-                </div>
+                </Link>
                 <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
                   Happy Changelog is an open workflow framework. Feel free to
                   adapt and implement it in your projects.
